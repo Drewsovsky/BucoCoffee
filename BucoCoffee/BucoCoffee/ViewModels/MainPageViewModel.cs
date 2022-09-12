9ec173a0362d435bac600b2f957d5491b08621f4
xdrew <attentionhub@gmail.com>
2022-09-12 23:04:43 +0300

    Added context binding

---
+++ BucoCoffee/BucoCoffee/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BucoCoffee.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel() 
+        {
+        }
+
+        public ICommand GotoNewItemPageCommand => new Command(GotoNewItemPage);
+
+        private void GotoNewItemPage()
+        {
+            Console.WriteLine("+++");
+        }
+    }
+}
