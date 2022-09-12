04506fcf760ef46719fd5bcded32f598fe4412ec
xdrew <attentionhub@gmail.com>
2022-09-12 22:33:40 +0300

    Added NewItemView

---
+++ BucoCoffee/BucoCoffee/Views/NewItemView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BucoCoffee.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewItemView : ContentPage
+    {
+        public NewItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
