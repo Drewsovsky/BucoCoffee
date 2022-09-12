9a337371bd56299ab4f7dbbeb6cc28696c7acd0a
xdrew <attentionhub@gmail.com>
2022-09-13 02:29:41 +0300

    Added package item models

---
+++ BucoCoffee/BucoCoffee/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace BucoCoffee.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public Key SelectedKey { get; set; }
+        public string PackingDate { get; set; } // DateTime
+        public string PackageDate { get; set; } // DateTime
+        public string PackerName { get; set; }
+        public int PackageAmount { get; set; }
+        public int Weight { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ BucoCoffee/BucoCoffee/Models/Key.cs
@@ -0,0 +1,8 @@
+﻿namespace BucoCoffee.Models
+{
+    public class Key
+    {
+        public string Title { get; set; }
+        public string ThemeColor { get; set; }
+    }
+}
