f149528ac6830fce2259e6fd802e691f229adadd
xdrew <attentionhub@gmail.com>
2022-09-13 14:26:44 +0300

    Added productType list to product page

--- BucoCoffee/BucoCoffee/ViewModels/NewItemPageViewModel.cs
+++ BucoCoffee/BucoCoffee/ViewModels/NewItemPageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BucoCoffee.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 
@@ -7,9 +9,20 @@ namespace BucoCoffee.ViewModels
 {
     public class NewItemPageViewModel : BaseViewModel
     {
+        public ObservableCollection<ProductType> ProductTypesList { get; set; }
+
         public NewItemPageViewModel(INavigation navigation)
         {
             Navigation = navigation;
+        }
+
+        public async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ProductTypesList = new ObservableCollection<ProductType>(await _firebaseHelper.GetAllProductTypes());
+
+            OnPropertyChanged(nameof(ProductTypesList));
         }
     }
 }
