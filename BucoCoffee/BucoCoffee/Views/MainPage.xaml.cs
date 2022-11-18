893d9918c7d389917831576acb9841225b031511
xdrew <attentionhub@gmail.com>
2022-11-18 13:02:16 +0200

    Added MessaginCenter functional to update prev page after adding new items into DB

--- BucoCoffee/BucoCoffee/Views/MainPage.xaml.cs
+++ BucoCoffee/BucoCoffee/Views/MainPage.xaml.cs
@@ -12,6 +12,12 @@ namespace BucoCoffee
             BindingContext = new MainPageViewModel(Navigation);
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            (BindingContext as MainPageViewModel).OnAppearing();
+        }
+
         private void SfButtonCalendar_Clicked(object sender, System.EventArgs e)
         {
             dummyDatePicker.Focus();
