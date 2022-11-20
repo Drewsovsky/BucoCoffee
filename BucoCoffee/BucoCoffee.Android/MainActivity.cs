1d713976d8f8e6e9bd8125c59f9192b06a526b93
xdrew <attentionhub@gmail.com>
2022-11-20 18:07:12 +0200

    Added remove image UI

--- BucoCoffee/BucoCoffee.Android/MainActivity.cs
+++ BucoCoffee/BucoCoffee.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.CrossPlatformTintedImage.Android;
 
 namespace BucoCoffee.Droid
 {
@@ -16,6 +17,7 @@ namespace BucoCoffee.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            TintedImageRenderer.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
